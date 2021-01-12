de8d2fb75c5b0dbe1e502a1cbf0e8cebe4d2cb01
Su Ho <su@nimblehq.co>
2021-01-12 16:51:24 +0700

    Init project by running `pod lib create` command

---
+++ Example/NimbleLocalAuthentication/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  NimbleLocalAuthentication
+//
+//  Created by 19943832 on 01/12/2021.
+//  Copyright (c) 2021 19943832. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
