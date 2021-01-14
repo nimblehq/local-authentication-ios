b3319961c87dfc769d46d548d0d705b1c590de11
Su Ho <su.v@nimblehq.co>
2021-01-14 16:43:37 +0700

    [#6] Create login screen

--- Example/Example/Sources/Modules/Login/LoginViewController.swift
+++ Example/Example/Sources/Modules/Login/LoginViewController.swift
@@ -12,9 +12,10 @@ final class LoginViewController: UIViewController {
   private let backgroundImageView = UIImageView()
   private let logoImageView = UIImageView()
   private let stackView = UIStackView()
-  private let emailField = UITextField()
-  private let passField = UITextField()
+  private let emailField = TextField()
+  private let passField = TextField()
   private let loginButton = UIButton()
+  private let biometricButton = UIButton()
 
   override var preferredStatusBarStyle: UIStatusBarStyle { .lightContent }
 
@@ -23,6 +24,11 @@ final class LoginViewController: UIViewController {
     setUpLayout()
     setUpView()
   }
+}
+
+// MARK: - Setup
+
+extension LoginViewController {
 
   private func setUpLayout() {
     view.addSubview(backgroundImageView)
@@ -30,14 +36,60 @@ final class LoginViewController: UIViewController {
     view.addSubview(stackView)
     stackView.addArrangedSubview(emailField)
     stackView.addArrangedSubview(passField)
+    stackView.addArrangedSubview(biometricButton)
     stackView.addArrangedSubview(loginButton)
 
-    backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+    [
+      backgroundImageView,
+      logoImageView,
+      stackView,
+      emailField,
+      passField,
+      loginButton,
+      biometricButton
+    ].forEach {
+      $0.translatesAutoresizingMaskIntoConstraints = false
+    }
+
     NSLayoutConstraint.activate([
       backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor),
       backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
       backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
       backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+    ])
+
+    NSLayoutConstraint.activate([
+      logoImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 150.0),
+      logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+    ])
+
+    stackView.spacing = 20.0
+    stackView.layoutMargins = UIEdgeInsets(top: 0.0, left: Constant.padding, bottom: 0.0, right: Constant.padding)
+    NSLayoutConstraint.activate([
+      stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+      stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+    ])
+
+    NSLayoutConstraint.activate([
+      emailField.heightAnchor.constraint(equalToConstant: Constant.fieldHeight),
+      emailField.widthAnchor.constraint(equalTo: stackView.widthAnchor, constant: -Constant.padding)
+    ])
+
+    NSLayoutConstraint.activate([
+      passField.heightAnchor.constraint(equalToConstant: Constant.fieldHeight),
+      passField.widthAnchor.constraint(equalTo: stackView.widthAnchor, constant: -Constant.padding)
+    ])
+
+    NSLayoutConstraint.activate([
+      loginButton.heightAnchor.constraint(equalToConstant: Constant.fieldHeight),
+      loginButton.widthAnchor.constraint(equalTo: stackView.widthAnchor, constant: -Constant.padding)
+    ])
+
+    NSLayoutConstraint.activate([
+      biometricButton.heightAnchor.constraint(equalToConstant: Constant.buttonSize.height),
+      biometricButton.widthAnchor.constraint(equalToConstant: Constant.buttonSize.width)
     ])
   }
 
@@ -45,5 +97,33 @@ final class LoginViewController: UIViewController {
     view.backgroundColor = .white
 
     backgroundImageView.image = UIImage(named: "LoginBackground")
+    logoImageView.image = UIImage(named: "WhiteLogo")
+    stackView.alignment = .center
+    stackView.axis = .vertical
+    stackView.isLayoutMarginsRelativeArrangement = true
+
+    emailField.placeholderText = "Email"
+    passField.placeholderText = "Password"
+
+    loginButton.setTitle("Login", for: .normal)
+    loginButton.backgroundColor = .white
+    loginButton.setTitleColor(.black, for: .normal)
+    loginButton.layer.cornerRadius = 12.0
+    loginButton.clipsToBounds = true
+
+    biometricButton.setImage(UIImage(named: "ic-face-id")?.withRenderingMode(.alwaysTemplate), for: .normal)
+    biometricButton.tintColor = .white
+  }
+}
+
+// MARK: - Constant
+
+extension LoginViewController {
+
+  enum Constant {
+
+    static let padding: CGFloat = 24.0
+    static let fieldHeight: CGFloat = 56.0
+    static let buttonSize: CGSize = .init(width: 44.0, height: 44.0)
   }
 }
