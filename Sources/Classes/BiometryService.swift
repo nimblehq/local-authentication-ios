9a0bcc010ff59ee51a3893a1d52975f30a5e845d
Su Ho <su@nimblehq.co>
2021-02-04 16:13:57 +0700

    [#11] Add document for biometry service

--- Sources/Classes/BiometryService.swift
+++ Sources/Classes/BiometryService.swift
@@ -16,15 +16,50 @@ public protocol BiometryService: AnyObject {
   /// A boolean value stating whether the biometric feature is available.
   var isAvailable: Bool { get }
 
-  /// A enum value stating
+  /// Indicates the type of the biometry supported by the device.
   var supportedType: BiometryType { get }
+
+  /// Fallback button title.
   var localizedFallbackTitle: String? { get set }
+
+  /// Cancel button title.
   var localizedCancelTitle: String? { get set }
+
+  /// Allows setting the default localized authentication reason on context.
   var localizedReason: String { get set }
+
+  /// Allows running authentication in non-interactive mode.
   var interactionNotAllowed: Bool { get set }
 
+  /// Authenticates the user when using the application.
+  ///
+  /// @param completion Completeion block that is executed when authentication finishes.
+  ///              success Completion parameter that is success if the authentication has been authenticate successfully.
+  ///              failure Completion parameter that is failed if the authentication has been authenticate failed.
+  ///                    contains error information about the authentication failure.
+  ///
+  /// @see BiometryError
   func authenticate(completion: @escaping (BiometryResult) -> Void)
+
+  /// Invalidates the biometry service
   func invalidate()
+
+  /// Credential provided by application
+  ///
+  /// Sets a credential to this service
+  ///
+  /// @param credential Credential to be used with subsequent calls. Setting this parameter to nil will remove
+  ///                   any existing credential of the specified type.
+  ///
+  /// @param type CredentialType of the provided credential.
+  ///
+  /// @return YES if the credential was set successfully, NO otherwise.
   func setCredential(_ credential: Data?, type: CredentialType) -> Bool
+
+  /// Reveals if credential was set with this context.
+  ///
+  /// @param type CredentialType of credential we are asking for.
+  ///
+  /// @return YES on success, NO otherwise.
   func isCredentialSet(_ type: CredentialType) -> Bool
 }
